9d7547587033bcdaba2fae2a6eec90b0e4e28aeb
Anwesh43 <anweshthecool0@gmail.com>
2019-07-18 11:52:00 +0530

    adding LSDNode class

--- linesquaredotview/src/main/java/com/anwesh/uiprojects/linesquaredotview/LineSquareDotView.kt
+++ linesquaredotview/src/main/java/com/anwesh/uiprojects/linesquaredotview/LineSquareDotView.kt
@@ -136,4 +136,47 @@ class LineSquareDotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LSDNode(var i : Int, val state : State = State()) {
+        private var next : LSDNode? = null
+        private var prev : LSDNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = LSDNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLSDNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LSDNode {
+            var curr : LSDNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
