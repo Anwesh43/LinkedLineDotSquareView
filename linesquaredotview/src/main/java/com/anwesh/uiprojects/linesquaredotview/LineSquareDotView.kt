a89b8eb7a6cdc734c4c036f0c09ab77637298ddc
Anwesh43 <anweshthecool0@gmail.com>
2019-07-18 11:45:21 +0530

    adding lineSquareDotView class

--- linesquaredotview/src/main/java/com/anwesh/uiprojects/linesquaredotview/LineSquareDotView.kt
+++ linesquaredotview/src/main/java/com/anwesh/uiprojects/linesquaredotview/LineSquareDotView.kt
@@ -73,3 +73,21 @@ fun Canvas.drawLSDNode(i : Int, scale : Float, paint : Paint) {
     drawLinesSquareDot(sc1, sc2, size, paint)
     restore()
 }
+
+class LineSquareDotView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
